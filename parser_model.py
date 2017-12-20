0c1536c047151f29d3a651b8ca6831d82bf8adfd
jeffrey1hu <hu1chao@126.com>
2017-12-20 11:39:50 +0800

    change some parameters

--- parser_model.py
+++ parser_model.py
@@ -1,13 +1,8 @@
-import os
-import time
 import tensorflow as tf
-import cPickle
 
 from model import Model
 from utils.general_utils import Progbar
-from utils.parser_utils import minibatches, load_and_preprocess_data
-
-from config import Config
+from utils.parser_utils import minibatches
 
 class ParserModel(Model):
     """
@@ -238,54 +233,5 @@ class ParserModel(Model):
         self.pretrained_embeddings = pretrained_embeddings
         self.config = config
         self.build()
-
-
-def main(debug=True):
-    print 80 * "="
-    print "INITIALIZING"
-    print 80 * "="
-    config = Config()
-    parser, embeddings, train_examples, dev_set, test_set = load_and_preprocess_data(debug)
-    if not os.path.exists('./data/weights/'):
-        os.makedirs('./data/weights/')
-
-    with tf.Graph().as_default():
-        print "Building model...",
-        start = time.time()
-        model = ParserModel(config, embeddings)
-        parser.model = model
-        print "took {:.2f} seconds\n".format(time.time() - start)
-
-        init = tf.global_variables_initializer()
-        # If you are using an old version of TensorFlow, you may have to use
-        # this initializer instead.
-        # init = tf.initialize_all_variables()
-        saver = None if debug else tf.train.Saver()
-
-        with tf.Session() as session:
-            parser.session = session
-            session.run(init)
-
-            print 80 * "="
-            print "TRAINING"
-            print 80 * "="
-            model.fit(session, saver, parser, train_examples, dev_set)
-
-            if not debug:
-                print 80 * "="
-                print "TESTING"
-                print 80 * "="
-                print "Restoring the best model weights found on the dev set"
-                saver.restore(session, './data/weights/parser.weights')
-                print "Final evaluation on test set",
-                UAS, dependencies = parser.parse(test_set)
-                print "- test UAS: {:.2f}".format(UAS * 100.0)
-                print "Writing predictions"
-                with open('q2_test.predicted.pkl', 'w') as f:
-                    cPickle.dump(dependencies, f, -1)
-                print "Done!"
-
-if __name__ == '__main__':
-    main(False)
 
 
---
+++ train.py
@@ -0,0 +1,57 @@
+import os
+import time
+import cPickle
+import tensorflow as tf
+
+from config import Config
+from parser_model import ParserModel
+from utils.parser_utils import load_and_preprocess_data
+
+
+def main(debug=True):
+    print 80 * "="
+    print "INITIALIZING"
+    print 80 * "="
+    config = Config()
+    parser, embeddings, train_examples, dev_set, test_set = load_and_preprocess_data(debug)
+    if not os.path.exists('./data/weights/'):
+        os.makedirs('./data/weights/')
+
+    with tf.Graph().as_default():
+        print "Building model...",
+        start = time.time()
+        model = ParserModel(config, embeddings)
+        parser.model = model
+        print "took {:.2f} seconds\n".format(time.time() - start)
+
+        init = tf.global_variables_initializer()
+        # If you are using an old version of TensorFlow, you may have to use
+        # this initializer instead.
+        # init = tf.initialize_all_variables()
+        saver = None if debug else tf.train.Saver()
+
+        with tf.Session() as session:
+            parser.session = session
+            session.run(init)
+
+            print 80 * "="
+            print "TRAINING"
+            print 80 * "="
+            model.fit(session, saver, parser, train_examples, dev_set)
+
+            if not debug:
+                print 80 * "="
+                print "TESTING"
+                print 80 * "="
+                print "Restoring the best model weights found on the dev set"
+                saver.restore(session, './data/weights/parser.weights')
+                print "Final evaluation on test set",
+                UAS, dependencies = parser.parse(test_set)
+                print "- test UAS: {:.2f}".format(UAS * 100.0)
+                print "Writing predictions"
+                with open('q2_test.predicted.pkl', 'w') as f:
+                    cPickle.dump(dependencies, f, -1)
+                print "Done!"
+
+if __name__ == '__main__':
+    main(False)
